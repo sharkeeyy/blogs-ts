c0c27b60f09b7ecfc2ec4cfbaab4c7b831b2f164
sharkeeyy <sfmei2003@yandex.ru>
2023-07-11 11:50:00 +0300

    started frontend

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Container from '@mui/material/Container';
+
+import { Header } from './components';
+import { Home, FullPost, Registration, AddPost, Login } from './pages';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/*<FullPost />*/}
+          {/*<AddPost />*/}
+          {/*<Login />*/}
+          {/*<Registration />*/}
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
