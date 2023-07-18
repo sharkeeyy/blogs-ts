919568ff752b2cf88c826e56981a49784bbbb0eb
sharkeeyy <sfmei2003@yandex.ru>
2023-07-18 10:15:05 +0300

    posts view

--- server/index.js
+++ server/index.js
@@ -40,6 +40,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 });
 
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
